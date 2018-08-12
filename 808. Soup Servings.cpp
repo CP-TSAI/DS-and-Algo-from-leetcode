6bb3f7f44484e595ae3864affb0ba62277993ee7
CP-Tsai <aupt23@gmail.com>
2018-08-12 13:52:56 -0400

    Add files via upload

---
+++ 808. Soup Servings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double soupServings(int N) {
+        int n = ceil(N / 25.0);
+        return N >= 4800 ? 1.0 : f(n, n);
+    }
+    
+    
+public:
+    double memo[200][200];
+    
+    double f(int a, int b){
+        if(a <= 0 && b <= 0) return 0.5;
+        if(a <= 0) return 1;
+        if(b <= 0) return 0;
+        if(memo[a][b] > 0) return memo[a][b];
+        memo[a][b] = 0.25 * (f(a-4, b) + f(a-3, b-1) + f(a-2, b-2) + f(a-1, b-3));
+        return memo[a][b];
+    }
+};
+
+// ref: https://blog.csdn.net/magicbean2/article/details/79851345
