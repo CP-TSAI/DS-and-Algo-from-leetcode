7f522f12cbcafb3169cb1378132f092ea5e93d6e
cptsai <aupt23@gmail.com>
2018-09-01 11:11:22 -0400

    162 edited: a cleaner code

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size()==1) return 0;
+        if(nums.size() == 1) return 0;
         for(int i = 0; i < nums.size(); i++){
-            if(i == 0 && nums[i+1] < nums[i]) return i;
-            if(i == nums.size()-1 && nums[i] > nums[i-1]) return i;
-            if(i != 0 && i != nums.size()-1 && nums[i-1] < nums[i] && nums[i+1] < nums[i]) return i;
+            if(i == 0){
+                if(nums[i] > nums[i + 1]) return i;
+            }
+            else if(i == nums.size() - 1){
+                if(nums[i] > nums[i - 1]) return i;
+            }
+            else{
+                if(nums[i-1] < nums[i] && nums[i] > nums[i+1]) return i;
+            }
         }
     }
 };
