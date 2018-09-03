fa21773ad6caee04710f958b87df47eb5043efea
cptsai <aupt23@gmail.com>
2018-09-03 10:10:57 -0400

    392 edited: add another method

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -11,3 +11,22 @@ public:
         return iter_s == s.end() ? true : false;
     }
 };
+
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         if(s.size() == t.size() && s != t) return false;
+//         int idx = 0;
+//         for(int i = 0; i < s.size(); i++){
+//             while(idx < t.size()){
+//                 if(s[i] == t[idx]){
+//                     idx++;
+//                     break;
+//                 }
+//                 else idx++;
+//             }
+//             if(idx == t.size() && i != s.size() - 1) return false;
+//         }
+//         return true;
+//     }
+// };
