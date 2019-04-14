633b4cf72717de497f77589806f0c52c0401225b
cptsai <aupt23@gmail.com>
2019-04-14 11:07:53 -0400

    766 edited

--- 766. Toeplitz Matrix.cpp
+++ 766. Toeplitz Matrix.cpp
@@ -1,33 +1,12 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        for(int i = 0; i < row; i++){
-            if(!isSame(i, 0, matrix)) return false;
-        }
-        for(int j = 0; j < col; j++){
-            if(!isSame(0, j, matrix)) return false;
+        if (matrix.size() == 1 || matrix[0].size() == 1) return true;
+        for(int i = 1; i < matrix.size(); i++) {
+            for(int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] != matrix[i-1][j-1]) return false;
+            }
         }
         return true;
     }
-    
-    bool isSame(int i, int j, vector<vector<int>>& matrix){
-        int idx_i = i+1;
-        int idx_j = j+1;
-        while(isValid(idx_i, idx_j, matrix)){
-            if(matrix[idx_i][idx_j] != matrix[i][j]) return false;
-            idx_i += 1;
-            idx_j += 1;
-        }
-        return true;
-    }
-    
-    bool isValid(int i, int j, vector<vector<int>>& matrix){
-        int row = matrix.size();
-        int col = matrix[0].size();
-        if(0 <= i && i < row && 0 <= j && j < col) return true;
-        else return false;
-    }
-    
 };
